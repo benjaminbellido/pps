5e26b1228b2e71184e8d4d96efed57b119140981
benja bellido <111450273+benjaminbellido@users.noreply.github.com>
2023-10-27 01:38:15 -0300

    s
    s

--- client/js/body.js
+++ client/js/body.js
@@ -2,7 +2,7 @@ const carruselItems = document.querySelectorAll('.carrusel-item');
 const btnAnterior = document.getElementById('btnAnterior');
 const btnSiguiente = document.getElementById('btnSiguiente');
 const carru_item = document.querySelectorAll('.carru-item');
-const botonAnterior = document.getElementById('btoonAnterior');
+const botonAnterior = document.getElementById('botonAnterior');
 const botonSiguiente = document.getElementById('botonSiguiente');
 let index = 0;
 let intervalId; // Variable para almacenar el intervalo de cambio automático
@@ -76,60 +76,5 @@ document.getElementById("menu-toggle").addEventListener("click", function () {
     }
 });
 
-function cambiarImagenSiguiente() {
-    index = (index + 1) % carruselItems.length;
-    mostrarItem(index);
-}
+// bloque noticia
 
-function cambiarImagenAnterior() {
-    index = (index - 1 + carruselItems.length) % carruselItems.length;
-    mostrarItem(index);
-}
-
-botonAnterior.addEventListener('click', () => {
-    cambiarImagenAnterior();
-});
-
-botonSiguiente.addEventListener('click', () => {
-    cambiarImagenSiguiente();
-});
-
-// Mostrar el primer elemento al cargar la página
-mostrarItem(index);
-// Agregar eventos de mouse para mostrar/ocultar las flechas al pasar el puntero
-const carrusel_noticia = document.querySelector('.carrusel_noticia');
-
-carrusel_noticia.addEventListener('mouseenter', () => {
-    botonAnterior.style.display = 'block';
-    botonSiguiente.style.display = 'block';
-    // Detener el intervalo de cambio automático cuando el puntero está sobre el carrusel
-    clearInterval(intervalId);
-});
-
-carrusel_noticia.addEventListener('mouseleave', () => {
-    botonAnterior.style.display = 'none';
-    botonSiguiente.style.display = 'none';
-    // Reanudar el intervalo de cambio automático cuando el puntero se retira del carrusel
-    iniciarIntervalo();
-});
-
-// Función para iniciar el intervalo de cambio automático cada 5 segundos
-function iniciarIntervalo() {
-    intervalId = setInterval(cambiarImagenSiguiente, 5000);
-}
-
-// Iniciar el intervalo de cambio automático al cargar la página
-iniciarIntervalo();;
-
-document.getElementById("menu-toggle").addEventListener("click", function () {
-    var mainNav = document.getElementById("main-nav");
-    var overlay = document.getElementById("overlay");
-    this.classList.toggle("active");
-    if (mainNav.style.display === "block") {
-        mainNav.style.display = "none";
-        overlay.style.display = "none"; // Oculta la capa semitransparente
-    } else {
-        mainNav.style.display = "block";
-        overlay.style.display = "block"; // Muestra la capa semitransparente
-    }
-});
